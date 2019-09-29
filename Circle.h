c306ca20f82af52ba1b6f106ec3fb531e68d39f1
BigBossMonchas <55475303+Big-Boss-Monchas@users.noreply.github.com>
2019-09-29 17:59:06 -0600

    Update Circle.h

--- Circle.h
+++ Circle.h
@@ -1,51 +1,59 @@
 #pragma once
-
 #include "Shapes.h"
 
 class Circle : public Shapes {
 private:
-	int radius;
-	int diameter;
+	float radius;
+	float diameter;
 	const double PI = 3.141592;
 
 public:
 	Circle() {
-		setPosition(0, 0);
+		setOrigin(0, 0);
 		setColor(1.0, 1.0, 1.0);
 		setLineWidth(1);
 		setRadius(25);
 
 	}
 
-	Circle(int x, int y) {
-		setPosition(x, y);
+	Circle(float x, float y) {
+		setOrigin(x, y);
 		setColor(1.0, 1.0, 1.0);
 		setLineWidth(1);
 		setRadius(25);
 	}
 
-	Circle(int x, int y, int rad) {
-		setPosition(x, y);
+	Circle(float x, float y, float rad) {
+		setOrigin(x, y);
 		setColor(1.0, 1.0, 1.0);
 		setLineWidth(1);
 		setRadius(rad);
 	}
 
-	Circle(int x, int y, int rad, float r, float g, float b) {
-		setPosition(x, y);
+	Circle(float x, float y, float rad, float r, float g, float b) {
+		setOrigin(x, y);
 		setColor(r, g, b);
 		setLineWidth(1);
 		setRadius(rad);
 	}
 
-	Circle(int x, int y, int rad, float r, float g, float b, int lwidth) {
-		setPosition(x, y);
+	Circle(float x, float y, float rad, float r, float g, float b, int lwidth) {
+		setOrigin(x, y);
 		setColor(r, g, b);
 		setLineWidth(lwidth);
 		setRadius(rad);
 	}
 
-	void setRadius(int rad) {
+	Circle(const Circle& circle) {
+		Vertex pos = circle.getOrigin();
+		setOrigin(pos.getX(), pos.getY());
+		Color col = circle.getColor();
+		setColor(col.getR(), col.getG(), col.getB());
+		setLineWidth(circle.getLineWidth());
+		setRadius(circle.getRadius());
+	};
+
+	void setRadius(float rad) {
 		if (rad > 0) { radius = rad; }
 		else { radius = 50; }
 		diameter = getRadius() * 2;
@@ -53,21 +61,21 @@ public:
 		perimeter = calculatePerimeter();
 	}
 
-	int getRadius() const { return radius; }
-	int getDiameter() const { return diameter; }
+	float getRadius() const { return radius; }
+	float getDiameter() const { return diameter; }
 
 	double calculateArea() { return (PI * (pow(radius, 2))); }
 	double calculatePerimeter() { return (2 * PI * radius); }
 
 	void drawShape() {
 		Color aux_color = getColor();
+		glColor3f(aux_color.getR(), aux_color.getG(), aux_color.getB());
 		Vertex aux_pos;
 		aux_pos.setX(0);
 		aux_pos.setY(getRadius());
-		glColor3f(aux_color.colorR, aux_color.colorG, aux_color.colorB);
 		glPointSize(getLineWidth());
 		float midpoint = 1 - radius;
-		circlePlotPoints(position.getX(), position.getY(), aux_pos);
+		circlePlotPoints(origin.getX(), origin.getY(), aux_pos);
 		while (aux_pos.getX() < aux_pos.getY())
 		{
 			aux_pos.setX(aux_pos.getX() + 1);
@@ -81,7 +89,7 @@ public:
 				aux_pos.setY(aux_pos.getY() - 1);
 				midpoint += 2 * (aux_pos.getX() - aux_pos.getY()) + 1;
 			}
-			circlePlotPoints(position.getX(), position.getY(), aux_pos);
+			circlePlotPoints(origin.getX(), origin.getY(), aux_pos);
 		}
 	}
 
@@ -102,10 +110,10 @@ public:
 
 	void drawShapeTrig() {
 		Color aux_color = getColor();
-		Vertex aux_pos = getPosition();
-		glColor3f(aux_color.colorR, aux_color.colorG, aux_color.colorB);
+		glColor3f(aux_color.getR(), aux_color.getG(), aux_color.getB());
+		Vertex aux_pos = getOrigin();
 		glLineWidth(getLineWidth());
-		int circle_points = 100;
+		int circle_points = 360;
 		glBegin(GL_LINE_LOOP);
 		for (int i = 0; i < circle_points; i++)
 		{
@@ -114,11 +122,16 @@ public:
 		}
 		glEnd();
 	}
-	void scaleShape(int _x, int _y) {
-		/*vertices[0].scaleVertex(_x, _y);
-		vertices[1].scaleVertex(_x, _y);
-		vertices[2].scaleVertex(_x, _y);
-		vertices[3].scaleVertex(_x, _y);*/
-	}
-};
 
+	void moveShape(float _x, float _y) {
+		origin.moveVertex(_x, _y);
+	}
+
+	void scaleShape(float factor) {
+		setRadius(factor * getRadius());
+	}
+	/*
+	void rotateShape(float _x, float _y, double _theta) {
+		
+	}*/
+};
